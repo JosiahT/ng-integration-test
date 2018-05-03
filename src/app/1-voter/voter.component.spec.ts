5b2812ee6ff176c1a95979f74913b0955ecdd5b0
Eyosiyas Tadesse <e.tadesse@lb.lashback.com>
2018-05-03 16:20:23 -0500

    added components

---
+++ src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { VoterComponent } from './voter.component';
+
+describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+  });
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges(); // we need to explicitly tell angular to detect changes
+
+    // query the DOM to get the tag that displays total votes with the fixture wrapper
+    let de = fixture.debugElement.query(By.css('.vote-count')); // wrapper for the native html element we queried
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up')); // wrapper for the native html element we queried
+    let el: HTMLElement = de.nativeElement;
+
+    expect(de.classes['highlighted']).toBeTruthy();
+  });
+});
